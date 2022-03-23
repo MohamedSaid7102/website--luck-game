58be7ee02c1e0f8c7a1e3cd1322fb9361b904a28
MohamedSaid7102 <mohamedsaidlearn@gmail.com>
2022-03-23 02:00:35 +0200

    initial commit with basic HTML & CSS

---
+++ public/script/index.js
@@ -0,0 +1,15 @@
+// Public Vars
+const maxScore = 20;
+
+// Public DOM Vars
+const maxScoreContainer = document.querySelector('.max-score');
+
+function gameInit() {
+  maxScoreContainer.textContent = maxScore;
+}
+
+// Manipulating dom
+
+// Main Invokation
+
+gameInit();
