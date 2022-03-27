615a586cc4c0fafd5d2ad86af34f91f9f1e6e036
MohamedSaid7102 <mohamedsaidlearn@gmail.com>
2022-03-27 02:29:54 +0200

    rolling dice behaviour & fixed some responsiveness issues

--- public/script/index.js
+++ public/script/index.js
@@ -1,36 +1,49 @@
 // Public Vars
 const maxScore = 20;
+let random = Math.floor(Math.random() * 7);
 
 // Public DOM Vars
 const maxScoreContainer = document.querySelector('.max-score');
+const diceImage = document.getElementById('dice');
+const player1FinalScore = document.getElementsByClassName(
+  'player1__final-score'
+)[0];
+const player2FinalScore = document.getElementsByClassName(
+  'player2__final-score'
+)[0];
+const player1CurrentScore = document.getElementsByClassName(
+  'player1__current-score'
+)[0];
+const player2CurrentScore = document.getElementsByClassName(
+  'player2__current-score'
+)[0];
 
 function gameInit() {
-  maxScoreContainer.textContent = maxScore;
+  rollingEffect();
+  maxScoreContainer.textContent = maxScore; //set maximum score
+  // reset all players score
+  player1FinalScore.textContent =
+    player2FinalScore.textContent =
+    player1CurrentScore.textContent =
+    player2CurrentScore.textContent =
+      '0.0';
 }
 
+function rollTheDice() {
+  random = Math.floor(Math.random() * 7);
+  diceImage.setAttribute('src', `./assets/images/dice-${random}.png`);
+  return random;
+}
+
+function rollingEffect() {
+  let rolling = setInterval(rollTheDice, 100);
+  setTimeout(() => {
+    clearInterval(rolling);
+    diceImage.setAttribute('src', `./assets/images/dice-img.png`);
+  }, 2000);
+}
 // Manipulating dom
 
 // Main Invokation
 
 gameInit();
-
-// Game Layout Edit scritp
-let browserWidth = document.documentElement.clientWidth;
-const newGameBtn = document.getElementById('new-game-btn');
-const rollBtn = document.getElementById('roll-btn');
-const holdBtn = document.getElementById('hold-btn');
-const mediaBreakPoint = 750;
-const editBtnContentOnResize = () => {
-  browserWidth = document.documentElement.clientWidth;
-  if (browserWidth <= mediaBreakPoint) {
-    newGameBtn.innerText = '🆕';
-    rollBtn.innerText = '🎲';
-    holdBtn.innerText = '🕹';
-  } else {
-    newGameBtn.innerText = '🆕 New Game';
-    rollBtn.innerText = '🎲 Roll';
-    holdBtn.innerText = '🕹 Hold';
-  }
-};
-window.addEventListener('resize', editBtnContentOnResize);
-editBtnContentOnResize();
